923842b020ce0e859dc89ed583ed79219245b5f0
amrimarihotjati <amrimarihotjati@gmail.com>
2023-09-29 01:01:00 +0700

    change layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilter } from "@fortawesome/free-solid-svg-icons";
 
 //API-Data
 interface ApiItem {
@@ -61,124 +62,116 @@ export default class Home extends Component<object, State> {
         }));
     }
 
-    render() {
-        return (
+    render(){
+        return(
             <React.Fragment>
-                {/* Filter */}
-                <div className='flex flex-wrap flex-row justify-center'>
-                    <div className='text-start mt-10 mb-1'>
-                        <h1 className='font-bold text-3xl text-indigo-600'>Filter</h1>
-                    </div>
-                </div>
-                <div className='flex flex-wrap flex-row justify-center'>
-                    <div className='flex flex-wrap flex-row justify-center'>
-                        <div className='flex flex-wrap flex-row justify-center'>
-                            {/* Search */}
-                            <div className="form-control ml-10 mr-10 mt-7">
-                                <div className="input-group">
-                                    <input type="text" placeholder="Search…" className="input input-bordered" />
-                                    <button className="btn btn-outline btn-success" onClick={(e) => this.handleSubmit(e)}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-                                    </button>
+                <div className='w-full h-screen bg-white flex'>
+                    <div className='flex-[5%] w-full h-full p-10'>
+                        {/* Title Filter */}
+                        <h1 className='text-3xl text-neutral-900 text-left font-semibold flex justify-start items-center gap-2'>
+                            <FontAwesomeIcon
+                                icon={faFilter}
+                                className='text-base text-white p-2 bg-indigo-900 rounded-full'
+                            />
+                            Filter
+                        </h1>
+                        <hr className="border border-neutral-700 rounded-full mt-5"></hr>
+                        {/* Search Button */}
+                        <div className="form-control mr-10 mt-7">
+                            <div className="join">
+                                <div>
+                                    <div>
+                                    <input className="input input-bordered join-item" placeholder="Search"/>
+                                    </div>
+                                </div>
+                                <div className="indicator">
+                                    <button className="btn join-item" onClick={(e) => this.handleSubmit(e)}>Search</button>
                                 </div>
                             </div>
-                            <div className='ml-10 mr-10'>
-                                <div className="collapse collapse-arrow mt-5 w-full border border-base-300 bg-base-200">
-                                    <input type="radio" name="my-accordion-2" />
-                                    <div className="collapse-title text-1xl text-neutral-900 text-center font-semibold">
-                                    Category
+                        </div>
+                        {/* Category */}
+                        <details className="collapse collapse-arrow border border-base-300 bg-base-200 mt-5">
+                            <summary className='collapse-title text-xl font-medium'>
+                                Category
+                            </summary>
+                            <div className='collapse-content'>
+                                {/* CheckBox Items */}
+                                <label className='label cursor-pointer justify-start gap-5'>
+                                    <input type="checkbox" className='checkbox checkbox-success' onChange={() => this.handleTagChange('Borgir')}/>
+                                    <span className='label-text text-neutral-900'>
+                                        Borgir
+                                    </span>
+                                </label>
+                                {/* CheckBox Items */}
+                                <label className='label cursor-pointer justify-start gap-5'>
+                                    <input type="checkbox" className='checkbox checkbox-success' onChange={() => this.handleTagChange('Bread')}/>
+                                    <span className='label-text text-neutral-900'>
+                                        Bread
+                                    </span>
+                                </label>
+                                {/* CheckBox Items */}
+                                <label className='label cursor-pointer justify-start gap-5'>
+                                    <input type="checkbox" className='checkbox checkbox-success' onChange={() => this.handleTagChange('Breakfast')}/>
+                                    <span className='label-text text-neutral-900'>
+                                        Breakfast
+                                    </span>
+                                </label>
+                            </div>
+                        </details>
+                        {/* Tag */}
+                        <details tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-200 mt-5">
+                            <summary className='collapse-title text-xl font-medium'>
+                                Tag
+                            </summary>
+                            <div className='collapse-content'>
+                                {/* CheckBox Items */}
+                                <label className='label cursor-pointer justify-start gap-5'>
+                                    <input type="checkbox" className='checkbox checkbox-secondary' onChange={() => this.handleCategoryChange('FAVORITE')}/>
+                                    <div className='badge badge-secondary'>
+                                        FAVORITE
                                     </div>
-                                        <div className="collapse-content">
-                                            <div className="form-control text-neutral-900 w-full">
-                                                <label className="label cursor-pointer justify-start gap-5">
-                                                <input
-                                                    type="checkbox"
-                                                    className="checkbox checkbox-success"
-                                                    onChange={() => this.handleTagChange('Borgir')}
-                                                />
-                                                <span className="label-text text-neutral-900">Borgir</span>
-                                                </label>
-                                            </div>
-                                            <div className="form-control text-neutral-900 w-full">
-                                                <label className="label cursor-pointer justify-start gap-5">
-                                                <input
-                                                    type="checkbox"
-                                                    className="checkbox checkbox-success"
-                                                    onChange={() => this.handleCategoryChange('Bread')}
-                                                />
-                                                <span className="label-text text-neutral-900">Bread</span>
-                                                </label>
-                                            </div>
-                                        </div>
-                                </div>  
+                                </label>
+                                {/* CheckBox Items */}
+                                <label className='label cursor-pointer justify-start gap-5'>
+                                    <input type="checkbox" className='checkbox checkbox-accent' onChange={() => this.handleCategoryChange('FEATURED')}/>
+                                    <div className='badge badge-accent'>
+                                        FEATURED
+                                    </div>
+                                </label>
+                                {/* CheckBox Items */}
+                                <label className='label cursor-pointer justify-start gap-5'>
+                                    <input type="checkbox" className='checkbox checkbox-primary' onChange={() => this.handleCategoryChange('NEW')}/>
+                                    <div className='badge badge-primary'>
+                                        NEW
+                                    </div>
+                                </label>
                             </div>
-                        </div>
-                        <div>
-                            <div className='ml-10 mr-10'>
-                                <div className="collapse collapse-arrow mt-5 w-full border border-base-300 bg-base-200">
-                                    <input type="radio" name="my-accordion-2" />
-                                    <div className="collapse-title text-1xl text-neutral-900 text-center font-semibold">Tag</div>
-                                        <div className="collapse-content">
-                                            <div className="form-control text-neutral-900 w-full">
-                                                <label className="label cursor-pointer justify-start gap-5">
-                                                <input
-                                                    type="checkbox"
-                                                    className="checkbox checkbox-secondary"
-                                                    onChange={() => this.handleTagChange('FAVORITE')}
-                                                />
-                                                <div className="badge badge-secondary">FAVOURITE</div>
-                                                </label>
-                                            </div>
-                                            <div className="form-control text-neutral-900 w-full">
-                                                <label className="label cursor-pointer justify-start gap-5">
-                                                <input type="checkbox" className="checkbox checkbox-accent"
-                                                onChange={() => this.handleTagChange('FEATURED')}
-                                                />
-                                                <div className="badge badge-accent">FEATURED</div>
-                                                </label>
-                                            </div>
-                                            <div className="form-control text-neutral-900 w-full">
-                                                <label className="label cursor-pointer justify-start gap-5">
-                                                <input
-                                                    type="checkbox"
-                                                    className="checkbox checkbox-primary"
-                                                    onChange={() => this.handleTagChange('NEW')}
-                                                />
-                                                <div className="badge badge-primary">NEW</div>
-                                                </label>
-                                            </div>
-                                        </div>
-                                </div>  
-                            </div>
-                        </div>
+                        </details>
                     </div>
-                </div>
-                <div>
-                    <div className='text-center mt-10 mb-10'>
-                        <h1 className='font-bold text-3xl text-indigo-600'>Cakes</h1>
-                    </div>
-                    {/* Card */}
-                    <div className='flex flex-wrap flex-row space-x-12 justify-center mb-20 ml-5 mr-5'>
-                    {/* Loop melalui data dummy dan render card untuk setiap objek */}
-                    {this.state.apiData.map((item) => (
-                        <div key={item.id} className="card w-96 bg-base-100 shadow-xl mt-10">
-                            <figure>
-                                <img src={item.imageUrl} alt={item.name} />
-                            </figure>
-                            <div className="card-body">
-                            <h2 className="card-title">
-                                {item.name}
-                                <div className="badge badge-secondary">{item.badge}</div>
-                            </h2>
-                            <p>{item.description}</p>
-                            <div className="card-actions justify-end">
-                                {item.categories.map((category) => (
-                                <div key={category} className="badge badge-outline">{category}</div>
-                                ))}
-                            </div>
-                            </div>
+                    <div className='flex-[95%] w-full h-full flex-wrap bg-slate-100 '>
+                        <div className='grid grid-cols-3 gap-4'>
+                            {this.state.apiData.map((item) => (
+                                <div className='m-5'>
+                                    <div key={item.id} className="card w-96 bg-base-100 shadow-xl mt-10">
+                                        <figure>
+                                            <img src={item.imageUrl} alt={item.name} />
+                                        </figure>
+                                        <div className="card-body">
+                                        <h2 className="card-title">
+                                            {item.name}
+                                            <div className="badge badge-secondary">{item.badge}</div>
+                                        </h2>
+                                        <p>{item.description}</p>
+                                        <div className="card-actions justify-end">
+                                            {item.categories.map((category) => (
+                                            <div key={category} className="badge badge-outline">{category}</div>
+                                            ))}
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
                         </div>
-                        ))}
                     </div>
                 </div>
             </React.Fragment>
