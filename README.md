73caf148faa1cd0b349be9611ae47621edc2a01e
amrimarihotjati <amrimarihotjati@gmail.com>
2023-10-02 00:49:57 +0700

    Switch to Chakra-UI

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-# Demo
-
-https://zippy-semolina-67b9f0.netlify.app/
-
 # React + TypeScript + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
-import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import RootLayout from './layouts/RootLayout'
+import PrivateRoute from "./components/PrivateRoutes";
+import Dashboard from './pages/Dashboard'
+import Create from './pages/Create'
+import Profile from './pages/Profile'
+import Detail from "./pages/Detail";
 
-
-
-export default class App extends React.Component{
-
-  render(){
-    return(
-      <React.Fragment>
-        <div>
-          <Home/>
-        </div>
-      </React.Fragment>
-    )
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<RootLayout/>}>
+              <Route index element={<Dashboard />} />
+            </Route>
+            <Route element={<PrivateRoute/>}>
+              <Route path="/" element={<RootLayout/>}>
+                <Route path="create" element={<Create />} />
+                <Route path="profile" element={<Profile />} />
+                <Route path="/detail/:id" element={<Detail />} />
+              </Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
   }
-  
 }
+
