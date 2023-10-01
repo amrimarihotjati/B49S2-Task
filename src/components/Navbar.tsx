034129d6f727256c3c497c15ef98fc9bc5f7c571
amrimarihotjati <amrimarihotjati@gmail.com>
2023-10-02 02:11:03 +0700

    Done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,27 +6,24 @@ import {
     Text,
     Button,
     Spacer,
-    HStack
+    HStack,
+    Image
 } from '@chakra-ui/react'
 
 export default class Navbar extends React.Component {
   render() {
     return (
-    <Flex flexWrap='wrap' as='nav' p='10px' alignItems='center'>
+    <Flex flexWrap='wrap' as='nav' p='20px' alignItems='center'>
         <Heading as={'h1'}>Cakes Menu</Heading>
         <Spacer/>
-        <HStack flexWrap='wrap'>
-            <Box bg={'gray.200'} p={'10px'}>M</Box>
+        <HStack flexWrap='wrap' gap='10px' mt='10px'>
+            <Box>
+              <Image w='30px' src='https://cdn-icons-png.flaticon.com/512/2919/2919906.png'/>
+            </Box>
             <Text>amrimarihotjati@gmail.com</Text>
             <Button colorScheme='purple'>Logout</Button>
         </HStack>
     </Flex>
-
-    //   <Flex bg={'gray.200'} justify='space-between' wrap='wrap' gap='5px'>
-    //     <Box w='150px' h='50px' background='red'>1</Box>
-    //     <Box w='150px' h='50px' flexGrow='1' bg='green' >2</Box>
-    //     <Box w='150px' h='50px' background='red'>3</Box>
-    //   </Flex>
     )
   }
 }
